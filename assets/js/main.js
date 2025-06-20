67641a812cf79ac38040a4034883d7d441581320
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2025-06-19 22:48:02 -0300

    Exibindo a lista após enviar a tarefa

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,25 @@ function app() {
     const formulario = document.querySelector('.form');
     const inputTarefa = formulario.querySelector('.tarefa');
     const btn = formulario.querySelector('.btn');
+    const tarefasUl = document.querySelector('.tarefas');
+
+
+    function criaLi() {
+        const li = document.createElement('li');
+        return li;
+    }
+
+    function criaTarefas(input) {
+        const li = criaLi();
+        li.innerText = input;
+        tarefasUl.appendChild(li);
+    }
 
     btn.addEventListener('click', function (e) {
          e.preventDefault();
          const valor = inputTarefa.value.trim();
          if(!valor) return;
-         console.log(valor)
+         criaTarefas(valor);
     });
 }
 
